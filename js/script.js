d044eeb53986becf965c1fe9ff7fc8141353747f
Kimberly Robinson <krobinson5@hawk.iit.edu>
2021-04-27 18:49:17 -0500

    Media Query and Map added

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+var map;
+function initMap() {	
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 41.87980015887943, lng: -87.62329487707916},
+    zoom: 2
+    ,center:map;
+  });
+}
+google.maps.event.addDomListenter(window, load 'initMap');
+
+
+
+		
+		
+		
+		
+		
+		
+		
