6e63cca673d94b22ce9b9d864b82f858f7c24865
Kimberly Robinson <krobinson5@hawk.iit.edu>
2021-05-04 12:36:37 -0500

    Adding markers to map
    Multiple attempts to add markers to map.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
 
-var map;
-function initMap() {	
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.87980015887943, lng: -87.62329487707916},
-    zoom: 2
-    ,center:map;
-  });
+
+
+ function init(){
+	//alert('it works');
+	var el = document.getElementById('map');
+	var myLocation = new google.maps.LatLng(41.87980015887943, -87.62329487707916);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 20,
+		
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		//animation: google.maps.Animation.BOUNCE,
+		
+	});
+
+	var contentString = '<img src="image/marker1.jpg" alt="#1 Site to See" width="300" height="30">';
+	var marker1= '41.87967013055939, -87.62373142891539';
+	var marker2= new google.maps.LatLng(41.8826870366143, -87.62343975521112);
+	var marker3= new google.maps.LatLng(41.882659304758576, -87.62330767610253);
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+	  
+
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
+
+
 }
-google.maps.event.addDomListenter(window, load 'initMap');
+
+google.maps.event.addDomListener(window, 'load', init);
 
 
 
